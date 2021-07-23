ceea37db199e6b16ea1ec17f639732616bd8fd25
Joe <jmleesjc2000@gmail.com>
2021-07-22 21:59:50 -0400

    Input delay

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-console.log('Hi there!');
-console.log('test')
+const getData = async (searchTerm) => {
+    const response = await axios.get('https://www.omdbapi.com/', {
+        params: {
+            apikey: 'ab8357e1',
+            s: searchTerm
+        }
+    });
+
+    console.log(response.data)
+}
+
+let timeoutID;
+
+const getInput = (e) => {
+    if(timeoutID) {
+        clearTimeout(timeout)
+    }
+    timeoutID = setTimeout(() => {
+        getData(e.target.value)
+    }, 1000)
+}
+
+const input = document.querySelector('input')
+input.addEventListener('keyup', getInput)
