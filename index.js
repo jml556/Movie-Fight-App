236ba4fc3add4659dfa6db98825be1b89bbcef69
Joe <jmleesjc2000@gmail.com>
2021-08-03 22:05:20 -0400

    Added dropdown list

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const getData = async (searchTerm) => {
     const response = await axios.get('https://www.omdbapi.com/', {
         params: {
@@ -6,19 +8,45 @@ const getData = async (searchTerm) => {
         }
     });
 
-    console.log(response.data)
-}
-
-let timeoutID;
-
-const getInput = (e) => {
-    if(timeoutID) {
-        clearTimeout(timeout)
+    if(response.data.Error) {
+        return [];
     }
-    timeoutID = setTimeout(() => {
-        getData(e.target.value)
-    }, 1000)
+
+    return response.data.Search
 }
 
+const root = document.querySelector('.autocomplete')
+
+root.innerHTML = `
+<label><b>Search for a Movie</b></label>
+<input class="input"/>
+<div class="dropdown">
+    <div class="dropdown-menu">
+        <div class="dropdown-content results">
+        </div>
+    </div>
+</div>
+`
 const input = document.querySelector('input')
-input.addEventListener('keyup', getInput)
+const dropdown = document.querySelector('.dropdown')
+const resultsWrapper = document.querySelector('.results')
+
+const getInput = async event => {
+    resultsWrapper.innerHTML = ''
+    const movies = await getData(event.target.value)
+    dropdown.classList.add('is-active')
+    for (let movie of movies) {
+        const option = document.createElement('a')
+        option.classList.add('dropdown-item')
+        const imgSrc = movie.Poster == 'N/A' ? '' : movie.Poster 
+
+        option.innerHTML = `
+            <img src="${imgSrc}">
+            ${movie.Title}
+        `
+        resultsWrapper.appendChild(option)
+    }
+
+}
+
+input.addEventListener('keyup', debounce(getInput))
